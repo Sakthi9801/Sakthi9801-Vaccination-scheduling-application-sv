c8e7ba3540d7782b429b6bc3f51a129b2efd9a20
SanSakthi9801 <sakthiviswanath520@gmail.com>
2024-06-25 11:18:00 +0530

    models added

--- mysite/vaccination/models.py
+++ mysite/vaccination/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from campaign.models import Campaign,Slot
 
-# Create your models here.
+User=get_user_model()
+class Vaccination(models.Model):
+    patient = models.ForeignKey(User,related_name="patient",on_delete =models.CASCADE)
+    campaign = models.ForeignKey(Campaign,on_delete=models.CASCADE)
+    slot=models.ForeignKey(Slot,on_delete=models.CASCADE)
+    date=models.DateField(null=True,blank=True)
+    is_vaccinated=models.BooleanField(default=False)
+    updated_me=models.ForeignKey(User,null=True,blank=True,on_delete=models.CASCADE)
+    updated_on=models.DateField(auto_now=True)        
+   
+    def __str__(self):
+       return self.patient.get_full_name() + " | " + str(self.campaign.vaccine.name)
+ 
--- mysite/vaccine/models.py
+++ mysite/vaccine/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Vaccine(models.Model):
+    name=models.CharField("vaccine name",max_length=32)
+    description=models.TextField(max_length=1024)
+    number_of_dosage=models.IntegerField(default=1)
+    intervel=models.IntegerField(default=0,help_text="pls provide interval in days")
+    storage_temperature=models.IntegerField(null=True,blank=True,help_text="provide the temperature")
+    minimun_age=models.IntegerField(default=0)
+                               
+    def __str__(self) -> str:
+        return self.name                          
